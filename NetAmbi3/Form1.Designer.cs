5614b9690f54f824a5298bde1da51971ee6f94df
Michael B <netopya@netopyaplanet.com>
2015-09-20 18:01:47 -0400

    Initial Commit
    Needs a lot of work doesn't it!

---
+++ NetAmbi3/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace NetAmbi3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(65, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(301, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(557, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(622, 149);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(622, 273);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 100);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(557, 379);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 100);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(301, 379);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(200, 100);
+            this.panel7.TabIndex = 2;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(65, 379);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(200, 100);
+            this.panel8.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(12, 241);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(200, 100);
+            this.panel9.TabIndex = 2;
+            // 
+            // panel10
+            // 
+            this.panel10.Location = new System.Drawing.Point(12, 135);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(200, 100);
+            this.panel10.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 195);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(358, 132);
+            this.textBox1.TabIndex = 4;
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 491);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ NetAmbi3/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO.Ports;
+
+namespace NetAmbi3
+{
+    public partial class Form1 : Form
+    {
+        Panel[] panels = new Panel[10];
+
+        Rectangle[] samples = new Rectangle[10];
+        Bitmap[] surfaces = new Bitmap[10];
+        Graphics[] graphics = new Graphics[10];
+        Color[] toColors = new Color[10];
+        Color[] currentColors = new Color[10];
+        SerialPort port; // = new SerialPort("COM3", 9600);
+
+        Graphics screen;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            samples[4] = new Rectangle(130, 100, 300, 120);
+            samples[5] = new Rectangle(690, 100, 300, 120);
+            samples[6] = new Rectangle(1250, 100, 300, 120);
+            samples[8] = new Rectangle(1560, 112, 120, 300);
+            samples[7] = new Rectangle(1560, 637, 120, 300);
+            samples[9] = new Rectangle(1250, 830, 300, 120);
+            samples[3] = new Rectangle(690, 830, 300, 120);
+            samples[0] = new Rectangle(130, 830, 300, 120);
+            samples[1] = new Rectangle(0, 637, 120, 300);
+            samples[2] = new Rectangle(0, 112, 120, 300);
+
+            surfaces[4] = new Bitmap(300, 120);
+            surfaces[5] = new Bitmap(300, 120);
+            surfaces[6] = new Bitmap(300, 120);
+            surfaces[8] = new Bitmap(120, 300);
+            surfaces[7] = new Bitmap(120, 300);
+            surfaces[9] = new Bitmap(300, 120);
+            surfaces[3] = new Bitmap(300, 120);
+            surfaces[0] = new Bitmap(300, 120);
+            surfaces[1] = new Bitmap(120, 300);
+            surfaces[2] = new Bitmap(120, 300);
+
+            for (int i = 0; i < currentColors.Length; i++)
+            {
+                currentColors[i] = Color.Black;
+                toColors[i] = Color.Black;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Color[] colors = new Color[10];
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = new Color();
+            }
+
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            int n = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < samples.Length; i++)
+                {
+                    graphics[i].CopyFromScreen(samples[i].X, samples[i].Y, 0, 0, samples[i].Size);
+
+                    r = 0;
+                    g = 0;
+                    b = 0;
+                    n = 0;
+
+                    for (int x = 0; x < samples[i].Width; x += 2)
+                    {
+                        for (int y = 0; y < samples[i].Height; y += 2)
+                        {
+                            Color pixel = surfaces[i].GetPixel(x, y);
+                            r += pixel.R;
+                            g += pixel.G;
+                            b += pixel.B;
+                            n++;
+                        }
+                    }
+
+                    r /= n;
+                    g /= n;
+                    b /= n;
+                    /*
+                    currentColors[i] = Color.FromArgb(
+                        incrementToVal(currentColors[i].R, (byte)r, 10),
+                        incrementToVal(currentColors[i].G, (byte)g, 10),
+                        incrementToVal(currentColors[i].B, (byte)b, 10)
+                    );*/
+
+                    colors[i] = Color.FromArgb(r, g, b);
+                }
+
+                //backgroundWorker1.ReportProgress(0, colors);
+
+
+                // here
+                panels[0].BackColor = colors[0];
+
+
+                byte[] buffer = new byte[30];
+
+                for (int i = 0; i < panels.Length; i++)
+                {
+                    panels[i].BackColor = colors[i];
+                    buffer[i * 3] = colors[i].R;// myLerp(0, 200,colors[i].R, 255);
+                    buffer[i * 3 + 1] = colors[i].G;
+                    buffer[i * 3 + 2] = colors[i].B;
+                }
+                /*
+                buffer = buffer.Select(t => (byte)0).ToArray();
+
+                buffer[0] = 255;// colors[0].R;
+                buffer[1] = 0; // colors[0].G;
+                buffer[2] = 0;// colors[0].B;
+                */
+                //textBox1.Text = String.Join(",", buffer);
+
+                SerialPort porta = new SerialPort("COM3", 9600);
+                porta.Open();
+                porta.Write(buffer, 0, buffer.Length);
+                porta.Close();
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panels[4] = panel1;
+            panels[5] = panel2;
+            panels[6] = panel3;
+            panels[8] = panel4;
+            panels[7] = panel5;
+            panels[9] = panel6;
+            panels[3] = panel7;
+            panels[0] = panel8;
+            panels[1] = panel9;
+            panels[2] = panel10;
+
+            for (int i = 0; i < graphics.Length; i++)
+            {
+                graphics[i] = Graphics.FromImage(surfaces[i]);
+            }
+
+            backgroundWorker1.RunWorkerAsync();
+            //backgroundWorker2.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //toColors = (Color[])e.UserState;
+
+            //return;
+            Color[] colors = (Color[])e.UserState;
+            
+            panels[0].BackColor = colors[0];
+
+
+            byte[] buffer = new byte[30];
+
+            for (int i = 0; i < panels.Length; i++)
+            {
+                panels[i].BackColor = colors[i];
+                buffer[i * 3] = colors[i].R;// myLerp(0, 200,colors[i].R, 255);
+                buffer[i * 3 + 1] = colors[i].G;
+                buffer[i * 3 + 2] = colors[i].B;
+            }
+            /*
+            buffer = buffer.Select(t => (byte)0).ToArray();
+
+            buffer[0] = 255;// colors[0].R;
+            buffer[1] = 0; // colors[0].G;
+            buffer[2] = 0;// colors[0].B;
+            */
+            textBox1.Text = String.Join(",",buffer);
+            
+            port.Open();
+            port.Write(buffer, 0, buffer.Length);
+            port.Close();
+        }
+
+        private byte myLerp(float min, float max, float amount, float oldMax)
+        {
+            return (byte)(min + (max - min) * (amount / oldMax));
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            byte[] buffer = new byte[30];
+
+            while (true)
+            {
+                for (int i = 0; i < currentColors.Length; i++)
+                {
+                    currentColors[i] = Color.FromArgb(
+                        incrementToVal(currentColors[i].R, toColors[i].R, 10),
+                        incrementToVal(currentColors[i].G, toColors[i].G, 10),
+                        incrementToVal(currentColors[i].B, toColors[i].B, 10)
+                    );
+
+                }
+
+                for (int i = 0; i < panels.Length; i++)
+                {
+                    //panels[i].BackColor = colors[i];
+                    buffer[i * 3] = currentColors[i].R;// myLerp(0, 200,colors[i].R, 255);
+                    buffer[i * 3 + 1] = currentColors[i].G;
+                    buffer[i * 3 + 2] = currentColors[i].B;
+                }
+
+                port.Open();
+                port.Write(buffer, 0, buffer.Length);
+                port.Close();
+
+                backgroundWorker2.ReportProgress(0);
+
+                Thread.Sleep(5);
+            }
+        }
+
+        private byte incrementToVal(byte currentVal, byte toVal, byte ratio)
+        {
+            //return (byte)(Math.Ceiling((double)(Math.Abs((currentVal - toVal) / ratio))) * Math.Sign(currentVal - toVal) + currentVal);
+            if (Math.Abs(toVal - currentVal) <= ratio)
+            {
+                return toVal;
+            }
+            return (byte)(currentVal + Math.Sign(toVal - currentVal)*ratio);
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //Color[] colors = (Color[])e.UserState;
+
+            for (int i = 0; i < panels.Length; i++)
+            {
+                panels[i].BackColor = currentColors[i];
+            }
+        }
+    }
+}
